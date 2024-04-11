54e506bed1bd7c03e00f3da12965f43d8878732e
Leonardo guevara <lguevara2805@outlook.com>
2024-04-11 11:31:16 -0400

    demo

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ const apiruta = require("./routes/ruta");
 
 const app = express();
 // port
-const PORT = process.env.PORT || 8080;
+
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use("/api",apiruta);
 
+
 app.listen(PORT,() => {
-    console.log("El puerto es :" + PORT);
+    console.log("El puerto es : http://localhost:" + PORT+'/api');
 })
 
