05f7e37176d5b7921e62cdc0d27d7539dad9cbda
Leonardo guevara <lguevara2805@outlook.com>
2024-04-18 22:00:20 -0400

    api

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -2,48 +2,41 @@ const enviarmensaje = require("../service/apiservice");
 
 const verificar = (req, res) => {
 
-    res.send('verficar');
-    // try{
-    //     var tokenandercode = "ANDERCODENODEJSAPIMETA";
-    //     var token = req.query["hub.verify_token"];
-    //     var challenge = req.query["hub.challenge"];
-
-    //     if (challenge != null && token != null && token == tokenandercode){
-    //         res.send(challenge);
-    //     }else{
-    //         res.status(400).send();
-    //     }
-
-    // }catch(e){
-    //     res.status(400).send();
-    // }
+    // res.send('verficar');
+    try{
+        var tokenfibocode = "FIBONODEJSAPIMETA";
+        var token = req.query["hub.verify_token"];
+        var challenge = req.query["hub.challenge"];
+        if (challenge != null && token != null && token == tokenfibocode){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
+    }catch(e){
+        res.status(400).send();
+    }
 
 }
 
 const recibir = (req, res) => {
+    // res.send('recibir');
+    try{
+        var entry = (req.body["entry"])[0];
+        var changes = (entry["changes"])[0];
+        var value = changes["value"];
+        var objetoMensaje = value["messages"];
 
-    res.send('recibir');
-
-    // try{
-    //     var entry = (req.body["entry"])[0];
-    //     var changes = (entry["changes"])[0];
-    //     var value = changes["value"];
-    //     var objetoMensaje = value["messages"];
-
-    //     if (typeof objetoMensaje != "undefined"){
-    //         var messages = objetoMensaje[0];
-
-    //         var texto = messages["text"]["body"];
-    //         var numero = messages["from"];
-
-    //         enviarmensaje.EnviarMensajeWhastpapp(texto,numero);
-    //     }
-
-    //     res.send("EVENT_RECEIVED");
-    // }catch(e){
-    //     console.log(e);
-    //     res.send("EVENT_RECEIVED");
-    // }
+        if (typeof objetoMensaje != "undefined"){
+            var messages = objetoMensaje[0];
+            var texto = messages["text"]["body"];
+            var numero = messages["from"];
+            enviarmensaje.EnviarMensajeWhastpapp(texto,numero);
+        }
+        res.send("EVENT_RECEIVED");
+    }catch(e){
+        console.log(e);
+        res.send("EVENT_RECEIVED");
+    }
 }
 
 module.exports = {
